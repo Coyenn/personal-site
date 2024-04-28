fe76ce1d6a85d98224f39dd38d014c54b382349b
Tim Ritter <timrittermail@gmail.com>
2024-04-28 12:47:07 +0200

    Attempt another fix

--- websites/www/src/app/(app)/[[...slug]]/page.tsx
+++ websites/www/src/app/(app)/[[...slug]]/page.tsx
@@ -12,12 +12,16 @@ interface PageProps {
 function getPageSlug(slug?: string | string[]) {
   let pageSlug = '/';
 
-  if (typeof slug === 'string' && slug !== 'index') {
+  if (typeof slug === 'string') {
     pageSlug = pageSlug + slug;
   }
 
   if (Array.isArray(slug)) {
     pageSlug = pageSlug + slug.join('/');
+  }
+
+  if (pageSlug === '/index') {
+    pageSlug = '/';
   }
 
   return pageSlug;
